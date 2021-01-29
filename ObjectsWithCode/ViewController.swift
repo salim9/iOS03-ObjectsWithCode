2e4f51a9e50c6a02bc0bafb5a26a8249c06b6a0b
salim9 <salimaniatv@gmail.com>
2021-01-29 20:41:27 +0300

    Initial Commit

---
+++ ObjectsWithCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ObjectsWithCode
+//
+//  Created by Salim Uzun on 29.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
